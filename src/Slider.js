75f21593a0be6783ab2fb38d8b47a9866f42b6db
harshitaMandwariya123 <harshitamandwariya@gmail.com>
2021-05-28 13:59:16 +0530

    add css and image

--- src/Slider.js
+++ src/Slider.js
@@ -1,15 +1,17 @@
+import "./slider.css";
+
 let Slider = () => {
     return (
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="/one.jpeg" class="d-block w-100" alt="..."/>
+            <img src="/one.jpeg" class="d-block w-100 Coursel-img" alt="one.jpeg"/>
           </div>
           <div class="carousel-item">
-            <img src="/two.jpeg" class="d-block w-100" alt="..."/>
+            <img src="/two.jpeg" class="d-block w-100 Coursel-img" alt="two.jpeg"/>
           </div>
           <div class="carousel-item">
-            <img src="/three.jpeg" class="d-block w-100" alt="..."/>
+            <img src="/three.jpeg" class="d-block w-100 Coursel-img" alt="three.jpeg"/>
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
