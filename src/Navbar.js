ee9d43ddc1c9a2fb2785acd4f786975582e50fc4
harshitaMandwariya123 <harshitamandwariya@gmail.com>
2021-05-28 18:08:15 +0530

    changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 let Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">HM</a>
+        <a class="navbar-brand" href="#"><i class="fa fa-birthday-cake" aria-hidden="true"></i></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -9,7 +9,7 @@ let Navbar = () => {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#"><b>Cake N Bake</b><span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">About</a>
@@ -22,9 +22,9 @@ let Navbar = () => {
                 Categories
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Men</a>
-                <a class="dropdown-item" href="#">Women</a>
-                <a class="dropdown-item" href="#">Kids</a>
+                <a class="dropdown-item" href="#">Birthdays</a>
+                <a class="dropdown-item" href="#">Anniversarys</a>
+                <a class="dropdown-item" href="#">Other</a>
               </div>
             </li>
           </ul>
